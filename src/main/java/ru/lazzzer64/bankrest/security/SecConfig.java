36563a535eed0013b0ca70747d8bc175531f25ab
lazzzer64 <lazzzer64@gmail.com>
2025-10-31 10:29:25 +0300

    feat: Временно отключил Spring Security

---
+++ src/main/java/ru/lazzzer64/bankrest/security/SecConfig.java
@@ -0,0 +1,23 @@
+package ru.lazzzer64.bankrest.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .anyRequest().permitAll()  // Разрешаем все запросы
+                )
+                .csrf(csrf -> csrf.disable())  // Отключаем CSRF
+                .headers(headers -> headers.disable()); // Отключаем security headers
+
+        return http.build();
+    }
+}
