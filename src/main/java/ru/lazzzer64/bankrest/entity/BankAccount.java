af895a5210454e45b03d86df504537a9301ce1b8
lazzzer64 <lazzzer64@gmail.com>
2025-11-06 12:38:18 +0300

    fix: Пофиксить проблемы

--- src/main/java/ru/lazzzer64/bankrest/entity/BankAccount.java
+++ src/main/java/ru/lazzzer64/bankrest/entity/BankAccount.java
@@ -12,12 +12,12 @@ public class BankAccount {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<Card> cards = new ArrayList<>();
+//    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+//    private List<Card> cards = new ArrayList<>();
 
-    @OneToOne()
-    @JoinColumn(name = "id")
-    private User user;
+//    @OneToOne()
+//    @JoinColumn(name = "id")
+//    private User user;
 
     //Геттеры и сеттеры
 
@@ -25,10 +25,10 @@ public class BankAccount {
     public BankAccount() {
     }
 
-    public BankAccount(String accountHolder, List<Card> cards, User user) {
-        this.cards = cards;
-        this.user = user;
-    }
+//    public BankAccount(String accountHolder, List<Card> cards, User user) {
+//        this.cards = cards;
+//        this.user = user;
+//    }
 
     public Long getId() {
         return id;
@@ -38,26 +38,26 @@ public class BankAccount {
         this.id = id;
     }
 
-    public List<Card> getCards() {
-        return cards;
-    }
-
-    public void setCards(List<Card> cards) {
-        this.cards = cards;
-    }
+//    public List<Card> getCards() {
+//        return cards;
+//    }
+//
+//    public void setCards(List<Card> cards) {
+//        this.cards = cards;
+//    }
 
 
 
     //Бизнес-логика
-    public void addCard(Card card) {
-        cards.add(card);
-        card.setAccount(this);
-    }
-
-    public void removeCard(Card card) {
-        cards.remove(card);
-        card.setAccount(null);
-    }
+//    public void addCard(Card card) {
+//        cards.add(card);
+//        card.setAccount(this);
+//    }
+//
+//    public void removeCard(Card card) {
+//        cards.remove(card);
+//        card.setAccount(null);
+//    }
 
 
 }
